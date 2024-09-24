4a82bbdfd39ee4bb268ea47835c4a5dc3decf393
Damian Piotrowski <kudlatydejmon@gmail.com>
2024-09-24 16:43:02 +0200

    m

--- clientGUI.py
+++ clientGUI.py
@@ -7,207 +7,225 @@ import sys
 import errno
 
 #Definiowanie podstawowych danych do połączenia między klientem a serwerem
-HOST = 'ring-s.hopto.org'
+HOST = '192.168.1.24'
 PORT = 2000
+PACKET_SIZE = 2048
 
 #Nawiązywanie połączenia między klientem a serwerem
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-user = ""
+class Client():
+    def __init__(self):
+        self.user = ""
+        self.running = True
+        
+        #Tworzenie aplikacji tkinter
+        self.app = Tk()
+        self.app.title("Wiadomości")
+        self.app.resizable(False, False)
 
-#Funkcja otwierająca dialog wyboru pliku
-def fileDialog():
-    filedialog.askopenfilename(title = "Wybierz plik do przesłania")
+        #Główna ramka interfejsu użytkownika
+        self.main_frame = ttk.Frame(self.app, padding = 5)
+        self.main_frame.grid()
 
-#Funkcja czyszcząca główną ramkę programu    
-def clearMainFrame():
-    for widget in main_frame.winfo_children():
-        widget.destroy()
+        #Przypisanie funkcji onClose do przycisku zamykającego okno programu
+        self.app.protocol("WM_DELETE_WINDOW", self.onClose)
+        
+        self.logIn()
+        self.app.mainloop()
 
-#Funkcja przesyłająca do serwera dane rejestracji
-def sendRegisterInfo(login_widget, password_widget, username_widget):
-    #Lista znaków dozwolonych przy rejestracji
-    allowed_chars = []
-    for i in range(65, 91):
-        allowed_chars.append(chr(i))
-    for i in range(97, 123):
-        allowed_chars.append(chr(i))
-    allowed_chars.extend(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-"])
-    
-    login = login_widget.get()
-    password = password_widget.get()
-    username = username_widget.get()
-    
-    if login != '' and password != '' and username != '':
-        if len(login) < 5 or len(login) > 16:
-            login_widget.delete(0, END)
-            return
-                
-        if len(password) < 5 or len(password) > 16:
-            password_widget.delete(0, END)
-            return
-                
-        if len(username) < 5 or len(username) > 16:
-            username_widget.delete(0, END)
-            return
-                
-        for char in login:
-            if char not in allowed_chars:
+    #Funkcja otwierająca dialog wyboru pliku
+    def fileDialog(self):
+        filepath = filedialog.askopenfilename(title = "Wybierz plik do przesłania")
+        filename = filepath.split("/")
+        filename = filename[len(filename) - 1]
+        print(filename)
+        
+        file = open(filepath, "rb")
+        
+        image_data = file.read(PACKET_SIZE)
+        self.write("[FILE]" + filename)
+        
+        while image_data:
+            client.send(image_data)
+            image_data = file.read(PACKET_SIZE)
+        
+        client.send(b"END_FILE")
+        file.close()
+
+    #Funkcja czyszcząca główną ramkę programu    
+    def clearMainFrame(self):
+        for widget in self.main_frame.winfo_children():
+            widget.destroy()
+
+    #Funkcja przesyłająca do serwera dane rejestracji
+    def sendRegisterInfo(self, login_widget, password_widget, username_widget):
+        #Lista znaków dozwolonych przy rejestracji
+        allowed_chars = []
+        for i in range(65, 91):
+            allowed_chars.append(chr(i))
+        for i in range(97, 123):
+            allowed_chars.append(chr(i))
+        allowed_chars.extend(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-"])
+        
+        login = login_widget.get()
+        password = password_widget.get()
+        username = username_widget.get()
+        
+        if login != '' and password != '' and username != '':
+            if len(login) < 5 or len(login) > 16:
                 login_widget.delete(0, END)
                 return
-                
-        for char in password:
-            if char not in allowed_chars:
+                    
+            if len(password) < 5 or len(password) > 16:
                 password_widget.delete(0, END)
                 return
-                
-        for char in username:
-            if char not in allowed_chars:
+                    
+            if len(username) < 5 or len(username) > 16:
                 username_widget.delete(0, END)
                 return
-                
-        info = "[REGISTER]" + login + " " + password + " " + username
-        write(info)
+                    
+            for char in login:
+                if char not in allowed_chars:
+                    login_widget.delete(0, END)
+                    return
+                    
+            for char in password:
+                if char not in allowed_chars:
+                    password_widget.delete(0, END)
+                    return
+                    
+            for char in username:
+                if char not in allowed_chars:
+                    username_widget.delete(0, END)
+                    return
+                    
+            info = "[REGISTER]" + login + " " + password + " " + username
+            self.write(info)
+            
+            self.logIn()
+
+    #Interfejs rejestrowania użytkownika
+    def register(self):
+        self.clearMainFrame()
         
-        logIn()
+        Label(self.main_frame, text = "Login", ).grid(column = 0, row = 0, sticky = "w")
+        Label(self.main_frame, text = "Hasło").grid(column = 0, row = 1, sticky = "w")
+        Label(self.main_frame, text = "Nazwa użytkownika").grid(column = 0, row = 2, sticky = "w")
+        
+        #Pola do wpisywania danych
+        login = Entry(self.main_frame, width = 30)
+        login.grid(column = 1, row = 0)
+        password = Entry(self.main_frame, width = 30)
+        password.grid(column = 1, row = 1)
+        username = Entry(self.main_frame, width = 30)
+        username.grid(column = 1, row = 2)
+        
+        #przycisk rejestracji
+        Button(self.main_frame, text = "Zarejestruj", command = lambda: self.sendRegisterInfo(login, password, username)).grid(column = 1, row = 3, sticky = "swen")
 
-#Interfejs rejestrowania użytkownika
-def register():
-    clearMainFrame()
-    
-    Label(main_frame, text = "Login", ).grid(column = 0, row = 0, sticky = "w")
-    Label(main_frame, text = "Hasło").grid(column = 0, row = 1, sticky = "w")
-    Label(main_frame, text = "Nazwa użytkownika").grid(column = 0, row = 2, sticky = "w")
-    
-    #Pola do wpisywania danych
-    login = Entry(main_frame, width = 30)
-    login.grid(column = 1, row = 0)
-    password = Entry(main_frame, width = 30)
-    password.grid(column = 1, row = 1)
-    username = Entry(main_frame, width = 30)
-    username.grid(column = 1, row = 2)
-    
-    #przycisk rejestracji
-    Button(main_frame, text = "Zarejestruj", command = lambda: sendRegisterInfo(login, password, username)).grid(column = 1, row = 3, sticky = "swen")
+    #Główny interfejs aplikacji
+    def mainApp(self):
+        self.clearMainFrame()
+        client.setblocking(0)
+        
+        #Ramka znajomych
+        friends_frame = Frame(self.main_frame, width = 200).grid(column = 0, row = 0, rowspan = 2)
+        
+        #Widżet Text, w którym wyświetlane są wiadomości odebrane z serwera
+        review_box = Text(self.main_frame)
+        review_box.grid(column = 1, row = 0, sticky = "swen", columnspan = 3)
+        review_box.configure(state='disabled')
+        
+        #Widżet Text, w którym użytkownik wpisuje wiadomość do wysłania
+        message_box = Entry(self.main_frame, width = 110)
+        message_box.grid(column = 1, row = 1, sticky = "we")
+        
+        #Przypisanie funkcji onEnterClick do klawisza enter
+        self.app.bind('<Return>', lambda e: self.onEnterClick(message_box))
+        
+        #Przyciski
+        Button(self.main_frame, text = "Wyślij", command = lambda: self.onEnterClick(message_box)).grid(column = 2, row = 1, sticky = "e")
+        Button(self.main_frame, text = "Wybierz plik...", command = self.fileDialog).grid(column = 3, row = 1, sticky = "e")
+        
+        #Wątek odbierający wiadomości z serwera
+        recv_thread = threading.Thread(target = lambda: self.receive(review_box))
+        recv_thread.start()
 
-#Główny interfejs aplikacji
-def mainApp():
-    clearMainFrame()
-    client.setblocking(0)
-    
-    #Ramka znajomych
-    friends_frame = Frame(main_frame, width = 200).grid(column = 0, row = 0, rowspan = 2)
-    
-    #Widżet Text, w którym wyświetlane są wiadomości odebrane z serwera
-    review_box = Text(main_frame)
-    review_box.grid(column = 1, row = 0, sticky = "swen", columnspan = 3)
-    review_box.configure(state='disabled')
-    
-    #Widżet Text, w którym użytkownik wpisuje wiadomość do wysłania
-    message_box = Entry(main_frame)
-    message_box.grid(column = 1, row = 1, sticky = "we")
-    
-    #Przypisanie funkcji onEnterClick do klawisza enter
-    app.bind('<Return>', lambda e: onEnterClick(message_box))
-    
-    #Przyciski
-    Button(main_frame, text = "Wyślij", command = lambda: onEnterClick(message_box)).grid(column = 2, row = 1)
-    Button(main_frame, text = "Wybierz plik...", command = fileDialog).grid(column = 3, row = 1)
-    
-    #Wątek odbierający wiadomości z serwera
-    recv_thread = threading.Thread(target = lambda: receive(review_box))
-    recv_thread.start()
+    def sendLogInInfo(self, login_widget, password_widget):
+        login = login_widget.get()
+        password = password_widget.get()
+        
+        if login != "" and password != "":
+            info = "[LOGIN]" + login + " " + password
+            self.write(info)
+        
+            response = client.recv(PACKET_SIZE).decode("utf-8")
+        
+            print(response)
+            if response == "[OK]":
+                self.user = response.replace("[OK]", "")
+                self.mainApp()
+            else:
+                return
+        
 
-def sendLogInInfo(login_widget, password_widget):
-    login = login_widget.get()
-    password = password_widget.get()
-    
-    if login != "" and password != "":
-        info = "[LOGIN]" + login + " " + password
-        write(info)
-    
-        response = client.recv(1024)
-    
-        print(response.decode("utf-8"))
-        if response.decode("utf-8") == "[OK]":
-            mainApp()
-        else:
-            return
-    
+    #Interfejs logowania użytkownika
+    def logIn(self):
+        self.clearMainFrame()
+        
+        Label(self.main_frame, text = "Login").grid(column = 0, row = 0, sticky = "w")
+        Label(self.main_frame, text = "Hasło").grid(column = 0, row = 1, sticky = "w")
+        
+        #Pola do wpisywania informacji
+        login = Entry(self.main_frame, width = 30)
+        login.grid(column = 1, row = 0, columnspan = 2)
+        password = Entry(self.main_frame, width = 30)
+        password.grid(column = 1, row = 1, columnspan = 2)
+        
+        #Przyciski
+        Button(self.main_frame, text = "Zaloguj", command = lambda: self.sendLogInInfo(login, password)).grid(column = 1, row = 2, sticky = "w")
+        Button(self.main_frame, text = "Zarejestruj", command = self.register).grid(column = 2, row = 2, sticky = "e")
 
-#Interfejs logowania użytkownika
-def logIn():
-    clearMainFrame()
-    
-    Label(main_frame, text = "Login").grid(column = 0, row = 0, sticky = "w")
-    Label(main_frame, text = "Hasło").grid(column = 0, row = 1, sticky = "w")
-    
-    #Pola do wpisywania informacji
-    login = Entry(main_frame, width = 30)
-    login.grid(column = 1, row = 0, columnspan = 2)
-    password = Entry(main_frame, width = 30)
-    password.grid(column = 1, row = 1, columnspan = 2)
-    
-    #Przyciski
-    Button(main_frame, text = "Zaloguj",command = lambda: sendLogInInfo(login, password)).grid(column = 1, row = 2, sticky = "w")
-    Button(main_frame, text = "Zarejestruj", command = register).grid(column = 2, row = 2, sticky = "e")
-
-#Funkcja wysyłająca wiadomość do serwera    
-def write(message):
-    try:
-        client.send(message.encode('utf-8'))
-    except:
-        print("Failed to connect to the server.")
-        client.close()
-
-#Funkcja odbierająca informacje od serwera    
-def receive(box):
-    global running
-    while running:
+    #Funkcja wysyłająca wiadomość do serwera    
+    def write(self, message = ""):
         try:
-            message = client.recv(1024)
-            message = message.decode('utf-8')
-            if message.startswith("[OK]") or message.startswith("[ERROR]"):
-                continue
-            else:
-                box.configure(state='normal')
-                box.insert(END, message)
-                box.insert(END, "\n")
-                box.configure(state='disabled')
-        except Exception as e:
-            if e.errno == errno.WSAEWOULDBLOCK:
-                continue
-            else:
-                client.close()
-                break
+            client.send(message.encode('utf-8'))
+        except:
+            print("Failed to connect to the server.")
+            client.close()
 
-#Funkcja odpowiadająca za kliknięcie przycisku enter
-def onEnterClick(box):
-    write(box.get())
-    box.delete(0, END)
-    return 'break'
+    #Funkcja odbierająca informacje od serwera    
+    def receive(self, box):
+        while self.running:
+            try:
+                message = client.recv(PACKET_SIZE)
+                message = message.decode('utf-8')
+                if message.startswith("[OK]") or message.startswith("[ERROR]"):
+                    continue
+                else:
+                    box.configure(state='normal')
+                    box.insert(END, message)
+                    box.insert(END, "\n")
+                    box.configure(state='disabled')
+            except Exception as e:
+                if e.errno == errno.WSAEWOULDBLOCK:
+                    continue
+                else:
+                    client.close()
+                    break
 
-#Funkcja odpowiadająca kliknięciu przycisku zamknięcia okna Tkinter
-def onClose():
-    global running
-    running = False
-    app.destroy()
+    #Funkcja odpowiadająca za kliknięcie przycisku enter
+    def onEnterClick(self, box):
+        self.write(box.get())
+        box.delete(0, END)
+        return 'break'
 
-#Stworzenie aplikacji tkinter
-app = Tk()
-app.title("Wiadomości")
+    #Funkcja odpowiadająca kliknięciu przycisku zamknięcia okna Tkinter
+    def onClose(self):
+        self.running = False
+        self.app.destroy()
 
-#Główna ramka interfejsu użytkownika
-main_frame = ttk.Frame(app, padding = 5)
-main_frame.grid()
-
-logIn()
-global running
-running = True
-
-#Przypisanie funkcji onClose do przycisku zamykającego okno programu
-app.protocol("WM_DELETE_WINDOW", onClose)
-
-#Główna pętla programu
-app.mainloop()
+if __name__ == "__main__":
+    client_app = Client()
--- serwer.py
+++ serwer.py
@@ -1,120 +1,141 @@
 import socket
 import threading
 
-HOST = '192.168.1.16'
+HOST = '192.168.1.24'
 PORT = 2000
+PACKET_SIZE = 2048
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-
 server.listen()
 
-clients = []
-logged_in = []
-users = []
+class Server:
+    def __init__(self):
+        self.clients = []
+        self.logged_in = []
+        self.users = []
+        
+        self.reloadUsers()
+        self.receive()
 
-def reloadUsers():
-    global users
-    users = []
-    try:
-        with open("users.txt", "r") as users_file:
-            for line in users_file.readlines():
-                user = {}
-                line = line.rstrip("\n")
-                line = line.split()
-                user["id"] = line[0]
-                user["login"] = line[1]
-                user["password"] = line[2]
-                user["username"] = line[3]
-                users.append(user)
-    except:
-        pass
-
-def broadcast(message, sender, username, send_address = True):
-    for client in logged_in:
-        if client is not sender:
-            if send_address:
-                client.send(("<" + username + ">" + "   " + message).encode('utf-8'))
-            else:
-                client.send(message.encode('utf-8'))
-        else:
-            if send_address:
-                client.send(f"<YOU>    {message}".encode('utf-8'))
-
-def handle(client, address):
-    username = ''
-    while True:
+    def reloadUsers(self):
+        self.users = []
         try:
-            message = client.recv(1024).decode('utf-8')
-            
-            if message.startswith("[REGISTER]"):
-                try:
-                    with open("uid.txt", "r") as id_file:
-                        last_id = int(id_file.readline())
-                        last_id += 1
-                except:
-                    last_id = 1
-                    
-                with open("uid.txt", "w") as id_file:
-                    id_file.write(str(last_id))
+            with open("users.txt", "r") as users_file:
+                for line in users_file.readlines():
+                    user = {}
+                    line = line.rstrip("\n")
+                    line = line.split()
+                    user["id"] = line[0]
+                    user["login"] = line[1]
+                    user["password"] = line[2]
+                    user["username"] = line[3]
+                    self.users.append(user)
+        except:
+            pass
+
+    def broadcast(self, message, sender, username, send_address = True):
+        for client in self.logged_in:
+            if client is not sender:
+                if send_address:
+                    client.send(("<" + username + ">" + "   " + message).encode('utf-8'))
+                else:
+                    client.send(message.encode('utf-8'))
+            else:
+                if send_address:
+                    client.send(f"<YOU>    {message}".encode('utf-8'))
+
+    def handle(self, client, address):
+        username = ''
+        while True:
+            try:
+                message = client.recv(PACKET_SIZE).decode('utf-8')
+                print(message)
                 
-                message = message.replace("[REGISTER]", "")
-                message = str(last_id) + " " + message + "\n"
-                
-                with open("users.txt", "a") as users_file:
-                    users_file.write(message)
+                if message.startswith("[REGISTER]"):
+                    allow = True
+                    try:
+                        with open("uid.txt", "r") as id_file:
+                            last_id = int(id_file.readline())
+                            last_id += 1
+                    except:
+                        last_id = 1
                     
-                reloadUsers()
-            
-            elif message.startswith("[LOGIN]"):
-                global users
-                send_error = True
-                message = message.replace("[LOGIN]", "")
-                message = message.split()
-                for user in users:
-                    if message[0] == user["login"]:
-                        if message[1] == user["password"]:
-                            username = user["username"]
-                            send_error = False
-                            logged_in.append(client)
-                            client.send("[OK]".encode("utf-8"))
+                    message = message.replace("[REGISTER]", "")
+                    register_info = message.split()
+                    message = str(last_id) + " " + message + "\n"
+                    
+                    for u in self.users:
+                        if register_info[0] == u["login"] or register_info[2] == u["username"]:
+                            allow = False
                             break
-                if send_error:
-                    client.send("[ERROR]".encode("utf-8"))
-            
-            elif not message:
-                if client in clients:
-                    clients.remove(client)
-                    if client in logged_in:
-                        logged_in.remove(client)
+                            
+                    if allow:
+                        with open("users.txt", "a") as users_file:
+                            users_file.write(message)
+                            
+                        with open("uid.txt", "w") as id_file:
+                            id_file.write(str(last_id))
+                            
+                        self.reloadUsers()
+                
+                elif message.startswith("[LOGIN]"):
+                    send_error = True
+                    message = message.replace("[LOGIN]", "")
+                    message = message.split()
+                    for user in self.users:
+                        if message[0] == user["login"]:
+                            if message[1] == user["password"]:
+                                username = user["username"]
+                                send_error = False
+                                self.logged_in.append(client)
+                                client.send("[OK]".encode("utf-8"))
+                                break
+                    if send_error:
+                        client.send("[ERROR]".encode("utf-8"))
+                
+                elif message.startswith("[FILE]"):
+                    filename = message.replace("[FILE]", "")
+                    with open(filename, "wb") as image_file:
+                        image_data = client.recv(PACKET_SIZE)
+
+                        while image_data != b"END_FILE":
+                            image_file.write(image_data)
+                            image_data = client.recv(PACKET_SIZE)
+                
+                elif not message:
+                    if client in self.clients:
+                        self.clients.remove(client)
+                        if client in self.logged_in:
+                            self.logged_in.remove(client)
+                        print(address[0] + " disconnected from the server.")
+                        client.close()
+                    break
+                    
+                else:
+                    self.broadcast(message, client, username)
+            except:
+                if client in self.clients:
+                    self.clients.remove(client)
+                    if client in self.logged_in:
+                        self.logged_in.remove(client)
                     print(address[0] + " disconnected from the server.")
                     client.close()
                 break
-                
-            else:
-                broadcast(message, client, username)
-        except:
-            if client in clients:
-                clients.remove(client)
-                if client in logged_in:
-                    logged_in.remove(client)
-                print(username + " disconnected from the server.")
-                client.close()
-            break
-        
-def receive():
-    while True:
-        try:
-            client, address = server.accept()
-        except:
-            break
             
-        print(f"{str(address[0])} connected to the server.")
-        clients.append(client)
-        
-        thread = threading.Thread(target = handle, args = (client, address))
-        thread.start()
-             
-print("Server running.")
-reloadUsers()
-receive()
+    def receive(self):
+        while True:
+            try:
+                client, address = server.accept()
+            except:
+                break
+                
+            print(f"{str(address[0])} connected to the server.")
+            self.clients.append(client)
+            
+            thread = threading.Thread(target = self.handle, args = (client, address))
+            thread.start()
+
+if __name__ == "__main__":          
+    print("Server running.")
+    server_app = Server()
